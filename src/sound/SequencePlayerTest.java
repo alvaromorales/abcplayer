338a0f61b972ecb98c97fa8056054b45c977c925
Alvaro Morales <alvarom@mit.edu>
2013-03-16 17:08:19 -0400

    Finished warmup exercises

--- src/sound/SequencePlayerTest.java
+++ src/sound/SequencePlayerTest.java
@@ -73,36 +73,49 @@ public class SequencePlayerTest {
         try {
             player = new SequencePlayer(200, 12);
             
-            player.addNote(new Pitch('C').toMidiNote(), 0, 12);
-            player.addNote(new Pitch('C').toMidiNote(), 12, 12);
-            player.addNote(new Pitch('C').toMidiNote(), 24, 9);
-            player.addNote(new Pitch('D').toMidiNote(), 33, 3);
-            player.addNote(new Pitch('E').toMidiNote(), 36, 12);
-            
-            player.addNote(new Pitch('E').toMidiNote(), 48, 9);
-            player.addNote(new Pitch('D').toMidiNote(), 57, 3);
-            player.addNote(new Pitch('E').toMidiNote(), 60, 9);
-            player.addNote(new Pitch('F').toMidiNote(), 69, 3);
-            player.addNote(new Pitch('G').toMidiNote(), 72, 24);
-            
-            player.addNote(new Pitch('C').transpose(Pitch.OCTAVE).toMidiNote(), 96, 4);
-            player.addNote(new Pitch('C').transpose(Pitch.OCTAVE).toMidiNote(), 100, 4);
-            player.addNote(new Pitch('C').transpose(Pitch.OCTAVE).toMidiNote(), 104, 4);
-            player.addNote(new Pitch('G').toMidiNote(), 108, 4);
-            player.addNote(new Pitch('G').toMidiNote(), 112, 4);
-            player.addNote(new Pitch('G').toMidiNote(), 116, 4);
-            player.addNote(new Pitch('E').toMidiNote(), 120, 4);
-            player.addNote(new Pitch('E').toMidiNote(), 124, 4);
-            player.addNote(new Pitch('E').toMidiNote(), 128, 4);
-            player.addNote(new Pitch('C').toMidiNote(), 132, 4);
-            player.addNote(new Pitch('C').toMidiNote(), 136, 4);
-            player.addNote(new Pitch('C').toMidiNote(), 140, 4);
+            player.addNote(new Pitch('F').transpose(1).toMidiNote(), 0, 6);
+            player.addNote(new Pitch('E').transpose(Pitch.OCTAVE).toMidiNote(), 0, 6);
+            player.addNote(new Pitch('F').transpose(1).toMidiNote(), 6, 6);
+            player.addNote(new Pitch('E').transpose(Pitch.OCTAVE).toMidiNote(), 6, 6);
+            //Z 6
+            player.addNote(new Pitch('F').transpose(1).toMidiNote(), 18, 6);
+            player.addNote(new Pitch('E').transpose(Pitch.OCTAVE).toMidiNote(), 18, 6);
+            //Z 6
+            player.addNote(new Pitch('F').transpose(1).toMidiNote(), 30, 6);
+            player.addNote(new Pitch('C').transpose(Pitch.OCTAVE).toMidiNote(), 30, 6);
+            player.addNote(new Pitch('F').transpose(1).toMidiNote(), 36, 12);
+            player.addNote(new Pitch('E').transpose(Pitch.OCTAVE).toMidiNote(), 36, 12);
 
-            player.addNote(new Pitch('G').toMidiNote(), 144, 9);
-            player.addNote(new Pitch('F').toMidiNote(), 153, 3);
-            player.addNote(new Pitch('E').toMidiNote(), 156, 9);
-            player.addNote(new Pitch('D').toMidiNote(), 165, 3);
-            player.addNote(new Pitch('C').toMidiNote(), 168, 24);
+            player.addNote(new Pitch('G').toMidiNote(), 48, 12);
+            player.addNote(new Pitch('B').toMidiNote(), 48, 12);
+            player.addNote(new Pitch('G').toMidiNote(), 48, 12);
+            //Z 12
+            player.addNote(new Pitch('G').toMidiNote(), 72, 12);
+            //Z 12
+
+            player.addNote(new Pitch('C').transpose(Pitch.OCTAVE).toMidiNote(), 96, 18);
+            player.addNote(new Pitch('G').toMidiNote(), 114, 6);
+            //Z 12
+            player.addNote(new Pitch('E').toMidiNote(), 132, 12);
+
+            player.addNote(new Pitch('E').toMidiNote(), 148, 6);
+            player.addNote(new Pitch('A').toMidiNote(), 154, 12);
+            player.addNote(new Pitch('B').toMidiNote(), 166, 12);
+            player.addNote(new Pitch('B').transpose(-1).toMidiNote(), 178, 6);
+            player.addNote(new Pitch('A').toMidiNote(), 184, 12);
+
+            player.addNote(new Pitch('G').toMidiNote(), 196, 8);
+            player.addNote(new Pitch('E').transpose(Pitch.OCTAVE).toMidiNote(), 204, 8);
+            player.addNote(new Pitch('G').transpose(Pitch.OCTAVE).toMidiNote(), 212, 8);
+            player.addNote(new Pitch('A').transpose(Pitch.OCTAVE).toMidiNote(), 220, 12);
+            player.addNote(new Pitch('F').transpose(Pitch.OCTAVE).toMidiNote(), 232, 6);
+            player.addNote(new Pitch('G').transpose(Pitch.OCTAVE).toMidiNote(), 238, 6);
+
+            //Z 6
+            player.addNote(new Pitch('E').transpose(Pitch.OCTAVE).toMidiNote(), 250, 12);
+            player.addNote(new Pitch('C').transpose(Pitch.OCTAVE).toMidiNote(), 262, 6);
+            player.addNote(new Pitch('D').transpose(Pitch.OCTAVE).toMidiNote(), 268, 6);
+            player.addNote(new Pitch('B').toMidiNote(), 274, 9);
             
             // play!
             player.play();
