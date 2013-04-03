b5ca7bc4dbe9d57689e74b4b2da1fbaf84b93f1a
Alvaro Morales <alvarom@mit.edu>
2013-04-02 20:51:15 -0400

    Modified player integration tests

--- src/test/PlayerVisitorTest.java
+++ src/test/PlayerVisitorTest.java
@@ -5,9 +5,12 @@ import org.junit.Test;
 import player.Main;
 
 /**
- * Tests the PlayerVisitor class
+ * Tests the PlayerVisitor class by testing the entire system together
+ * Main.play uses the Lexer, Parser, DurationVisitor and PlayerVisitor to play a song
  * Testing Strategy
  *  - play fur_elise.abc
+ *  - play invention.abc
+ *  - play little_night_music.abc
  *  @category no_didit
  */
 public class PlayerVisitorTest {
@@ -18,6 +21,36 @@ public class PlayerVisitorTest {
     @Test
     public void playFurEliseTest() {
         String filename="sample_abc/fur_elise.abc";
+        try{
+            Main.play(filename);
+        }
+        catch(IOException e){
+            System.err.println(e.getMessage());
+            System.exit(-1);
+        }
+    }
+    
+    /**
+     * Plays invention.abc
+     */
+    @Test
+    public void playInventionTest() {
+        String filename="sample_abc/invention.abc";
+        try{
+            Main.play(filename);
+        }
+        catch(IOException e){
+            System.err.println(e.getMessage());
+            System.exit(-1);
+        }
+    }
+    
+    /**
+     * Plays little_night_music.abc
+     */
+    @Test
+    public void playLittleNightMusicTest() {
+        String filename="sample_abc/little_night_music.abc";
         try{
             Main.play(filename);
         }
